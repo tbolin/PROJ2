25304bbe263a5b264bb2ab012259bf817ecc1fc6
tbolin <tobias_bolin@hotmail.com>
2013-11-28 17:20:34 +0100

    småjusteringar

--- derivera.m
+++ derivera.m
@@ -10,27 +10,31 @@ function Y=derivera(f, a, d)
     yn = numeriskt(f, a, d);
     yn2 = numeriskt2(f, a, d);
     ya = analytiskt(f, a);
-    Y = [yn; yn2 ; ya];
+    Y = double([yn; yn2 ; ya]);
 end
 
 %Derivera numeriskt
 function yn=numeriskt(f, a, d)
     h=0.0001;
-    yn = double((f(a+h)-f(a-h))/(2*h));
+    yn = vpa((f(a+h)-f(a-h))/(2*h));
     %yn = (double((f(a+h))-double(f(a-h))))/(2*h);
 end
 
 function yn=numeriskt2(f, a, d)
-    h=0.0001;
-    yn = double((8*(f(a+h)-f(a-h))-f(a+2*h)+f(a-2*h))/(12*h));
+    h=vpa(0.000000025);
+    yn = vpa((8*(f(a+h)-f(a-h))-f(a+2*h)+f(a-2*h))/(12*h));
+    %yn = (8*(vpa(f(a+h))-vpa(f(a-h)))-vpa(f(a+2*h))+vpa(f(a-2*h)))/(12*h);
 end
 
 %Derivera analytiskt
 function ya=analytiskt(f, a)
     der = diff(f);
-    ya = double(der(a));
+    ya = vpa(der(a));
 end
 
 % Det är 
 % syms x;
 % f(x) = sym('funktion')
+
+%f(x) = sym('x^3+0.1*x^2+13*x-22*cos(x)')
+ 
