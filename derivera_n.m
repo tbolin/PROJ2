337ca83b28a8723c2c5d8f94298ff1fa0f325466
tbolin <tobias_bolin@hotmail.com>
2013-12-05 19:29:50 +0100

    Felhantering för nolldivision
    Varnar och trixar om tal om den försöker dela med 0. Resultaten kommer
    fortfarande vara helt åt pipan, men programmet kracha i alla fall inte.

--- derivera_n.m
+++ derivera_n.m
@@ -14,28 +14,46 @@ function dy = derivera_n(f, a, b, n, varargin)
     intervall = linspace(a,b,n);	% formaterar intervallet
     start_steg = 1/(2^10);          % sätter ett lämpligt startsteg
     numeriskt = @numeriskt_exp4;    % väljer numerisk funktion
-    % Beräknar en lämplig steglängd
-    %prel_berr = numeriskt(f, intervall, start_steg);
-    nytt_steg = start_steg./bis(f, intervall, start_steg);  %justerar steglängd beroende på 2:a derivatan
-    ny_berr = numeriskt(f, intervall, nytt_steg);
+    % Beräknar en lämplig steglängd basserat på 2:1 derivatan
+    nytt_steg = start_steg./bis(f, intervall, start_steg); 
+    berr = numeriskt(f, intervall, nytt_steg);
     if nargin <= 4
-        dy = double(ny_berr);
+        dy = double(berr);
     else
-        dy = vpa(ny_berr);
+        dy = vpa(berr);
     end
 end
 
 function ddy=bis(f, a, h)
     % Beräknar andraderivata
     andra = (f(a+h)-2*f(a)+f(a-h))./(h.^2);
+    try
     if double(andra)
         ddy=andra;
     else
         ddy=0.00000000001;
     end
+    % Felhantering för nolldivision, kanske borde ligga högre upp i
+    % hierarkin
+    catch err
+        if (strcmp(err.identifier, 'MATLAB:nologicalnan')) && ~all(a)
+            smallest = 2^(-1022);
+            sprintf(['WARNING! Division by zero. \n Adjusting zero elements to ', num2str(smallest)])
+            for i=1:length(a)
+                if ~a(i)
+                    a(i) = smallest;
+                end
+            end
+            ddy = bis(f, a, h);
+        else
+            rethrow(err);
+        end 
+    end
 end
 
-% v RichardsonextrMAGI be here v
+%
+% RichardsonextrMAGI be here, borde ge ett fel prop mot h^10
+% Inte så snyggt, gör om till en rekursiv funktion om/när jag orkar
 function dy=numeriskt_exp(f, a, h)
     dy = (2*f(a+2*h)-2*f(a-2*h)-f(a+h)+f(a-h))./(2*h*(4-1));
 end
