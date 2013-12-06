b3bc976c23e0e8b54d54ca507f69f36f7c2166b1
ndless <jmn@octane.localdomain>
2013-12-06 20:13:11 +0100

    Anrop av meny+ritfunktion parameterstyrd, så man kan byta enkelt.

--- maingui.m
+++ maingui.m
@@ -8,12 +8,14 @@ clear all; close all;
 % (Defaultvärden sätts i classdef-filen)
 %
 p=parametrar;
-x=sym('x'); % Indikera att x ska hanteras som variabel
+
+% Indikera att x ska hanteras som analytisk/symbolisk variabel
+x=sym('x');
 
 running=1;
 
 while running
-    val=enkel_meny(p);
+    val=p.meny(p);
     switch val
         case 1
         case 2
@@ -21,35 +23,35 @@ while running
             p.text='Deriverat numeriskt';
             p.fx1handle = @derivera_n;
             p.fxcount = 1; % Ingen jämförelse
-            rita(p, val);
+            p.rita(p, val);
         case 4
             p.text='Deriverat analytiskt';
             p.fx1handle = @derivera_a;
             p.fxcount = 1; % Ingen jämförelse
-            rita(p, val);
+            p.rita(p, val);
         case 5
             p.text='Integrerat numeriskt';
             p.fx1handle = @integrera_n;
             p.fxcount = 1; % Ingen jämförelse
-            rita(p, val);
+            p.rita(p, val);
         case 6
             p.text='Integrerat analytiskt';
             p.fx1handle = @integrera_a;
             p.fxcount = 1; % Ingen jämförelse
-            rita(p, val);
+            p.rita(p, val);
         case 7
             p.text='Jämförelse numerisk/analytisk derivering';
             p.fx1handle = @derivera_n;
             p.fx2handle = @derivera_a;
             p.fxcount = 2;
-            rita(p, val);           
+            p.rita(p, val);           
         case 8
             p.text='Jämförelse numerisk/analytisk integrering';
             p.fx1handle = @integrera_n;
             p.fx2handle = @integrera_a;
             p.fxcount = 2;
-            rita(p, val);
-        case 9 
+            p.rita(p, val);
+        case 9
             running=0;
     end
     
