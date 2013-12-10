7fc55953d96a3dc24686edcdd3c9a1e752fa7357
tbolin <tbolin@kth.se>
2013-12-10 16:59:30 +0100

    Början på en mer generell felhantering

--- rita.m
+++ rita.m
@@ -10,16 +10,41 @@ function fel=rita(p, menyval)
 
     % Beräkna x, y och plotta
     x1 = linspace(p.xmin, p.xmax, p.xstep);
-    y1 = p.fx1handle(f, p.xmin, p.xmax, p.xstep);    
-    plot(x1, y1);
+    
+    try
+        y1 = p.fx1handle(f, p.xmin, p.xmax, p.xstep); 
+        plot(x1, y1);
+    % Felhantering för division med 0
+    catch err 
+        % kontrollerar feltyp
+        if (strcmp(err.identifier, 'MATLAB:nologicalnan'))
+            % Varnar för delning med 0
+            sprintf(['WARNING! Division by zero. \n Adjusting zero elements to ', num2str(smallest)])
+        else
+            % Om det är en annan slags fel skickas det vidare upp
+            rethrow(err);
+        end 
+    end
 
     title([p.text ' f(x)=' p.funktion]);
     
     if p.fxcount == 2 % Finns en till funktion att jämföra med
         hold on;
         % Beräkna den andra funktionen och plotta
-        y2 = p.fx2handle(f, p.xmin, p.xmax, p.xstep);
+    try
+        y2 = p.fx2handle(f, x1);
         plot(x1, y2, 'g');
+        % Felhantering för division med 0
+    catch err 
+        % kontrollerar feltyp
+        if (strcmp(err.identifier, 'MATLAB:nologicalnan'))
+            % Varnar för delning med 0
+            sprintf(['WARNING! Division by zero. \n Adjusting zero elements to ', num2str(smallest)])
+        else
+            % Om det är en annan slags fel skickas det vidare upp
+            rethrow(err);
+        end 
+    end
         
         % Beräkna fel^2 och plotta 
         plot(x1, (y1-y2).^2, 'r');
