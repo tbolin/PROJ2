9047be9463f0bbfc525285f90b4104b37ed8e868
MindThought <mindthought37@gmail.com>
2013-12-10 15:23:24 +0100

    Update integrera_a.m

--- integrera_a.m
+++ integrera_a.m
@@ -1,14 +1,12 @@
 function [ py ] = integrera_a(f, a, b, n)
-% Beräknar integralerna analytiskt
+% Berï¿½knar integralerna analytiskt
 % IN
 % f symbolisk funktion i en variabel
-% a start för beräkningsintervallet
-% b slut för beräkningsintervallet
+% a start fï¿½r berï¿½kningsintervallet
+% b slut fï¿½r berï¿½kningsintervallet
 % n antalet punkter i intervallet
 % UT
-% py vektor med de analytiskt beräknade integralerna
+% py vektor med de analytiskt berï¿½knade integralerna
     x = linspace(a, b, n);
-    int = diff(f);
-    py = vpa(int(x));
+    py = vpa(int(f(x)));
 end
-
