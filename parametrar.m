455c6f0c9c4d534f6db446f74b7acd85666cf8a3
tbolin <tobias_bolin@hotmail.com>
2013-12-11 23:04:23 +0100

    Nu då?

--- parametrar.m
+++ parametrar.m
@@ -1,26 +1,19 @@
 classdef parametrar < handle    % 'handle' <-> klassen hanteras med
-                                % pekare, så den kan modifieras av
+                                % pekare, sÃ¥ den kan modifieras av
                                 % funktioner den skickas till
                                 
-    % Ren dataklass med parametrar för ritjobbet, men ingen kod
+    % Ren dataklass med parametrar fÃ¶r ritjobbet, men ingen kod
     properties
         meny = @enkel_meny;     % Enkel meny som default
         rita = @rita;           % Enkel ritfunktion som default
-        text='';                % Etikett för plotten
+        text='';                % Etikett fÃ¶r plotten
         funktion='3*x^2+2';     % Funktion att jobba med
-        xmin = 0;               % minsta värde att undersöka
-        xmax = 10;              % största värde...
+        xmin = 0;               % minsta vÃ¤rde att undersÃ¶ka
+        xmax = 10;              % stÃ¶rsta vÃ¤rde...
         xstep = 2000;           % Antal punkter att plotta [xmin, xmax]
-<<<<<<< HEAD
-        feltr = 0.001;          % Tröskel för fel att markera i plot
-        fxcount = 0;            % Antal funktioner att jämföra
-        fx1handle = 0;          % Första funktion
-        fx2handle = 0;          % Andra funktion att jämföra med
-=======
         feltr = 0.001;          % TrÃ¶skel fÃ¶r fel att markera i plot
         fxcount = 0;            % Antal funktioner att jÃ¤mfÃ¶ra
         fx1handle = 0;          % FÃ¶rsta funktion
         fx2handle = 0;          % Andra funktion att jÃ¤mfÃ¶ra med
->>>>>>> origin/master
     end
 end
