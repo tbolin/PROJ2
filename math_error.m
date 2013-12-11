d43da1dc66b4963be1551f23cf0b054f498c22b6
tbolin <tobias_bolin@hotmail.com>
2013-12-11 22:09:31 +0100

    hantering för felaktiga fku
    Gör vad den heter

--- math_error.m
+++ math_error.m
@@ -26,19 +26,23 @@ function [ y, fel ] = math_error( p, f_no, varargin )
         % Hantering av olika typer av fel ska vara här!
     catch err 
         % Felhantering för division med 0
-        if strcmp(err.identifier, 'MATLAB:nologicalnan') ...
-                || strcmp(err.identifier,'symbolic:mupadmex:CommandError')
+        if strcmp(err.identifier, 'MATLAB:nologicalnan') || ...
+           strcmp(err.identifier,'symbolic:mupadmex:CommandError')
             % Varnar för delning med 0, men krachar inte programmet
             fel = 1;
             formatSpec = 'Varning! Funktionen delar med 0.\n Felkod: %i\n';
+        elseif strcmp(err.identifier, 'MATLAB:minrhs') || ...
+               strcmp(err.identifier, 'symbolic:sym:sym:errmsg9')
+            fel = 2;
+            formatSpec = 'Funktionen är felformaterad eller använder inte x som variabel\n Felkod: %i\n';
         else
             % Om det är ett fel som inte hanteras här skickas det vidare
             err.identifier
             fel = -1;
             rethrow(err);
         end
-        fprintf(2, formatSpec, fel)
-        y = 0;
+        fprintf(2, formatSpec, fel);
+        y = 0; % sätter y = 0 för att undvika kracher pga fel output
     end 
 end
 
