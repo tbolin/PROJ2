bf35f68b4ad5c50ec9ccb4ef76b4185b99b101df
MindThought <mindthought37@gmail.com>
2013-12-12 10:46:34 +0100

    Integrera

--- integrera_n.m
+++ integrera_n.m
@@ -1,15 +1,18 @@
 function py = integrera_n(f, a, b, n, varargin)
-% Beräknar integralerna numeriskt
+% Berï¿½knar integralerna numeriskt
 % IN
 % f symbolisk funktion i en variabel
-% a start för beräkningsintervallet
-% b slut för beräkningsintervallet
+% a start fï¿½r berï¿½kningsintervallet
+% b slut fï¿½r berï¿½kningsintervallet
 % n antalet punkter i intervallet
 % UT
-% py vektor med de numreriskt beräknade integralerna
-    intervall = linspace(a,b,n);	% formaterar intervallet
-    h = (b-a)/(n/2);                    % h = steglängden                          
-    py = ((intervall+h)-(intervall-h))*(f(intervall-h)+f(intervall+h)/2);   % Trapetsregeln
+% py vektor med de numreriskt berï¿½knade integralerna
+    %py = zeros(1,n)
+    syms k;
+    h = (b-a)/(n);                    % h = steglängden 
+    for N = [1:1:n],
+        py(N) = vpa(h*symsum((f(a+(k-1)*h)+f(a+(k-1)*h +h))/2, k, 1, N)); %trapetsregeln
+    end
 end
-% "vildvittror drakar och gråsuggor" - © Tobias
+% "vildvittror drakar och gråsuggor" - ï¿½ Tobias
 
