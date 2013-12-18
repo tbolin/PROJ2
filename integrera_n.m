e5340db057cbce4110607271c10d383ae33c33e6
MindThought <mindthought37@gmail.com>
2013-12-18 11:08:51 +0100

    Update integrera_n.m

--- integrera_n.m
+++ integrera_n.m
@@ -7,35 +7,23 @@ function py = integrera_n(f, a, b, n, varargin)
 % n antalet punkter i intervallet
 % UT
 % py vektor med de numreriskt berï¿½knade integralerna
-<<<<<<< HEAD
     syms k;
-    h = (b-a)/(n);                  % h = steglängden
+    h = (b-a)/(n);                  % h = steglï¿½ngden
     f = matlabFunction(f);
-    ay = zeros(1,n);                % förallokerar ay som används för att spara steglängden
-    for N = [1:1:n],                % loop som hittar steglängden
-        X = a+(N-1)*h;              % första x-värdet
-        Y = a+(N)*h;                % andra x-värdet
+    ay = zeros(1,n);                % fï¿½r allokerar ay som anvï¿½nds fï¿½r att spara steglï¿½ngden
+    for N = [1:1:n],                % loop som hittar steglï¿½ngden
+        X = a+(N-1)*h;              % fï¿½rsta x-vï¿½rdet
+        Y = a+(N)*h;                % andra x-vï¿½rdet
         ay(N) = vpa(h*(f(X)+f(Y))/2); %trapetsregeln
     end
     L = length(ay);
     py = zeros(1,L);                
     for K = linspace(1,L,L)         
         if K == 1
-            py(K) = ay(K);          % för att inte försöka hitta element 0
+            py(K) = ay(K);          % fï¿½r att inte fï¿½rsï¿½ka hitta element 0
         else
-            py(K) = vpa(ay(K) + py(K-1));   % sätter ihop steget med tidigare summa för en ny summa.
+            py(K) = vpa(ay(K) + py(K-1));   % sï¿½tter ihop steget med tidigare summa fï¿½r en ny summa.
         end
     end
 end
 % "vildvittror drakar och grï¿½suggor" - ï¿½ Tobias
-=======
-    %py = zeros(1,n)
-    syms k;
-    h = (b-a)/(n);                    % h = steglängden 
-    for N = [1:1:n],
-        py(N) = vpa(h*symsum((f(a+(k-1)*h)+f(a+(k-1)*h +h))/2, k, 1, N)); %trapetsregeln
-    end
-end
-% "vildvittror drakar och gråsuggor" - ï¿½ Tobias
->>>>>>> master
-
